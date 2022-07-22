754b71a21d469981e343e20294449031572f02da
Ramy Melo <ramysmelo@gmail.com>
2022-07-22 13:54:21 -0400

    chore: adds PRB test from node modules

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "@prb/test/PRBTest.sol";
 
 import "../src/Token.sol";
 
-contract TokenTest is Test {
+contract TokenTest is PRBTest {
     Token t;
 
     function setUp() public {
