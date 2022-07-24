e6e33bf53487393ab730c1ad5bda7f47d9f91f79
Ramy Melo <ramysmelo@gmail.com>
2022-07-23 23:22:55 -0400

    ref: extracted out mock into seperate contract.

--- test/CSVWallet.t.sol
+++ test/CSVWallet.t.sol
@@ -1,38 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {MockVotingToken} from "./mocks/MockVotingToken.sol";
 import {PRBTest} from "prb-test/PRBTest.sol";
 import {MinimalProxyFactory} from "solidstate-solidity/factory/MinimalProxyFactory.sol";
-import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "openzeppelin-contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import {ERC20Votes} from "openzeppelin-contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {CSVWallet} from "../src/CSVWallet.sol";
-
-contract MockToken is ERC20, ERC20Permit, ERC20Votes {
-    constructor() ERC20("MockToken", "MTK") ERC20Permit("MockToken") {}
-
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._burn(account, amount);
-    }
-}
 
 contract CSVWalletTest is PRBTest, MinimalProxyFactory {
     event DelegateChanged(
@@ -43,10 +15,10 @@ contract CSVWalletTest is PRBTest, MinimalProxyFactory {
 
     CSVWallet wallet;
     CSVWallet proxyWallet;
-    MockToken token;
+    MockVotingToken token;
 
     function setUp() public {
-        token = new MockToken();
+        token = new MockVotingToken();
         wallet = new CSVWallet(token);
         proxyWallet = CSVWallet(_deployMinimalProxy(address(wallet)));
         proxyWallet.initialize();
